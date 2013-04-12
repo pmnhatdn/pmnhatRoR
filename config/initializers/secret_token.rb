2b07ccb3a8bada706bcd6c3d8529d9e9a2ed4fb0
PHAM MINH NHAT <pmnhatdn@gmail.com>
2013-04-11 20:45:23 -0700

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pmnhat::Application.config.secret_token = 'e2ae8084bb3e962bdb07d568ee416b8f76511e1f0cc56dce445781873883c174570b4eea839bd256a4150b376808caf81803c2661ef757a7a9c054b422a0e61d'
